f95468c4c08adad98fd41d8a27091581725e08b9
SmyZzy <smyzzy@gmail.com>
2026-05-04 22:36:14 +0300

    Fix SSL: embed Aiven cert directly in pool.js
    - Removed ca.pem file (cert now embedded in code)
    - This fixes 'ca file not found' error on Render
    - Simplified SSL config for cloud deployment

--- database/pool.js
+++ database/pool.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
+
+const caCert = `-----BEGIN CERTIFICATE-----
+MIIERDCCAqygAwIBAgIUT/0t4XrXZQp2sDENRcwH7NwQ8x0wDQYJKoZIhvcNAQEM
+BQAwOjE4MDYGA1UEAwwvOWQ0NDg4MjctMDdkMy00NzU5LWEwYzctYjBjNDY0ODAx
+MmExIFByb2plY3QgQ0EgUmlnaHQgQXRlbGllciBBbXIgaW4gRmlubGFuZAJBgNVBAYT
+AkZpMQswCQYDVQQDDAI1ZDQ0ODgyNy0wN2QzLTQ3NTktYTBjNy1iMGM0NjQ4MDEyYTEg
+-----END CERTIFICATE-----`;
 
 const sslConfig = process.env.DB_SSL === 'true' ? {
-  ca: fs.readFileSync(path.join(__dirname, 'ca.pem'))
+  ca: caCert,
+  rejectUnauthorized: true
 } : null;
 
 const pool = mysql.createPool({
